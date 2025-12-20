d27501b03a1bf58ca5cfc2dc61a524fbcce80532
mridul bhardwaj <mridulbhardwaj13@gmail.com>
2025-12-20 12:24:41 +0530

    Fix React Router v6 layout and sidebar navigation

--- src/App.js
+++ src/App.js
@@ -1,74 +1,55 @@
 import "./App.css";
-import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./AuthProvider";
+import ProtectedRoute from "./ProtectedRoute";
+
+import PublicLayout from "./publicLayout";
+import DashboardLayout from "./DashboardLayout";
+
 import WeatherBanner from "./WeatherBanner";
 import ForgotPassword from "./ForgotPassword";
+import Logout from "./Logout";
+
 import LiveData from "./LiveData";
+import KisanChatbot from "./kesanAI/kesan";
+import WeeklyOverview from "./WeeklyOverview";
+import Export from "./Export";
 import Fungus from "./Fungus/Fungus";
 import Disease from "./Disease/Disease";
 import Pest from "./Pest/Pest";
 import Spray from "./Spray/Spray";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import Export from "./Export";
-import Logout from "./Logout";
-import { AuthProvider } from "./AuthProvider";
-import WeeklyOverview from "./WeeklyOverview";
 import User from "./User";
-import KisanChatbot from "./kesanAI/kesan";
-
-function Layout() {
-  const location = useLocation();
-  const path = location.pathname;
-
-  const showNavbar =
-    path === "/" || path === "/forgotpassword" || path === "/logout";
-  const showSidebar = !showNavbar;
-
-  return (
-    <>
-      {showNavbar && (
-        <>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<WeatherBanner />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-            <Route path="/logout" element={<Logout />} />
-          </Routes>
-        </>
-      )}
-
-      {showSidebar && (
-        <>
-          {/* <Sidebar /> */}
-
-          <div className="">
-            <Routes>
-              <Route path="/livedata" element={<LiveData />} />
-              <Route path="/kisan" element={< KisanChatbot />} />
-              <Route path="/export" element={<Export />} />
-              <Route path="/weekly" element={<WeeklyOverview />} />
-              <Route path="/user" element={<User />} />
-              <Route path="/fungus" element={<Fungus />} />
-              <Route path="/disease" element={<Disease />} />
-              <Route path="/pest" element={<Pest />} />
-              <Route path="/spray" element={<Spray />} />
-            </Routes>
-          </div>
-        </>
-      )}
-    </>
-  );
-}
 
 function App() {
   return (
     <Router basename="/station">
       <AuthProvider>
-        <Layout />
+        <Routes>
+
+          {/* PUBLIC ROUTES */}
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<WeatherBanner />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+
+          {/* PROTECTED ROUTES */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<DashboardLayout />}>
+              <Route path="/livedata" element={<LiveData />} />
+              <Route path="/kisan" element={<KisanChatbot />} />
+              <Route path="/weekly" element={<WeeklyOverview />} />
+              <Route path="/export" element={<Export />} />
+              <Route path="/fungus" element={<Fungus />} />
+              <Route path="/disease" element={<Disease />} />
+              <Route path="/pest" element={<Pest />} />
+              <Route path="/spray" element={<Spray />} />
+              <Route path="/user" element={<User />} />
+            </Route>
+          </Route>
+
+        </Routes>
       </AuthProvider>
     </Router>
   );
